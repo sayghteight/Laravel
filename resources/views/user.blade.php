713227e1dd07647cb4270092696ffad3a4621073
sayghteight <sayghteight@gmail.com>
2022-11-17 12:33:54 +0100

    chore: Fix typo error in view

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,7 +5,7 @@
         <div class="py-5">
         <h1 class="display-5 fw-bold text-white">CRUD EXAMPLE</h1>
         <div class="col-lg-6 mx-auto">
-            <p class="fs-5 mb-4">In this example you will be able to create users, modify them, delete them and update them.</p>
+            <p class="fs-5 mb-4">In this example you will be able to create/read/update/delete accounts.</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <a href="{{ route('users.create') }}" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Create new account</a>
             </div>
